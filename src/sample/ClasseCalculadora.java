5adb2c9cc39ffb71232cdee1e684e7cdd4ea53da
LucioHenrique512 <lucions96@gmail.com>
2018-08-05 14:32:44 -0300

    Melhorando o codigo
    Melhorando e comentando

--- src/sample/ClasseCalculadora.java
+++ src/sample/ClasseCalculadora.java
@@ -7,28 +7,23 @@ public class ClasseCalculadora {
     private double valordometroquadrado;
     private double valordometrolinear;
 
+    //Pegando o valor do da metragem linear.
     public void setMetragemLinear(String metragemlinear){
         this.metragemlinear = Double.parseDouble(metragemlinear);;
     }
-
+    //Pegando o valor total cobrado.
     public void setValorCobrado(String valorcobrado){
         this.valorcobrado = Double.parseDouble(valorcobrado);
     }
-
+    //Retornando valor da metragem quadrada.
     public String getMetroquadrado(){
         return String.valueOf(this.metroquadrado = this.metragemlinear * 1.4);
     }
-
-
-
-    public double getValorCobrado(){
-        return this.valorcobrado;
-    }
-
+    //Retornando o valor em reais do metro quadrado.
     public String getValordometroquadrado(){
-        return String.valueOf(this.valordometroquadrado = getValorCobrado() / this.metroquadrado);
+        return String.valueOf(this.valordometroquadrado = this.valorcobrado / this.metroquadrado);
     }
-
+    //Retornando o valor do metro linear em reais.
     public String getValordometrolinear(){
         return String.valueOf(this.valordometroquadrado * 1.4);
     }
